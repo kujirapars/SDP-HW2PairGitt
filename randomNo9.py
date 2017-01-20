5bb87a6ec8d1646cde4d2d35d1e8451f98088cb0
Pattarakhon057 <ar.pattarakhon_st@tni.ac.th>
2017-01-20 04:44:50 +0000

    workspace

--- randomNo9.py
+++ randomNo9.py
@@ -1,14 +1,14 @@
 import random
 
-num = random.randint(0,999999)
+num = random.randint(0,99999)*10+9
 
 print "1st Lotto 6 digit"
 print(num)
 
 
-num1 = random.randint(0,999)
-num2 = random.randint(0,999)
-num3 = random.randint(0,999)
+num1 = random.randint(0,99)*10+9
+num2 = random.randint(0,99)*10+9
+num3 = random.randint(0,99)*10+9
 
 print "1st Lotto 3 digit"
 print(num1)
@@ -17,7 +17,7 @@ print(num2)
 print "3rd Lotto 3 digit"
 print(num3)
 
-num4 = random.randint(0,99)
+num4 = random.randint(0,9)*10+9
 
 print "1st Lotto 2 digit"
-print(num)
+print(num4)
